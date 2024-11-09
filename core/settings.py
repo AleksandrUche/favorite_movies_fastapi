1350e8781493369a33652a837ba575c316bd9962
AleksandrUche <zak.reizer@mail.ru>
2024-11-09 16:33:51 +0300

    refactor: added data from env

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,12 @@
 import os
 
-from config import (
-    DB_USER, DB_NAME, DB_PASS, DB_HOST, DB_PORT, BASE_URL_KINOPOISK, API_KEY_KINOPOISK)
+from dotenv import load_dotenv
 
-SECRET_KEY = b"SECRET_KEY"
+load_dotenv()
+
 # Secret key
+SECRET_KEY = b"SECRET_KEY"
+
 if not SECRET_KEY:
     SECRET_KEY = os.urandom(32)
 
@@ -12,8 +14,14 @@ if not SECRET_KEY:
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
 
 # Data Base
+DB_USER = os.environ.get('DB_USER')
+DB_NAME = os.environ.get('DB_NAME')
+DB_PASS = os.environ.get('DB_PASS')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # KinopoiskApi
-BASE_URL_KINOPOISK = BASE_URL_KINOPOISK
-API_KEY_KINOPOISK = API_KEY_KINOPOISK
+BASE_URL_KINOPOISK = os.environ.get('BASE_URL_KINOPOISK')
+API_KEY_KINOPOISK = os.environ.get('API_KEY_KINOPOISK')
